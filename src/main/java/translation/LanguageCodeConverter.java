1eaf39a5c27b76acc5243bc2948c3d803d27fc78
houssamyaacoub <houssamyaacoub2@gmail.com>
2025-09-22 12:33:59 -0400

    Task A complete

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -43,6 +43,13 @@ public class LanguageCodeConverter {
             while (iterator.hasNext()) {
                 String line = iterator.next();
                 // TODO Task A: use line to populate the instance variables
+                String[] parts = line.split("\t");
+                String language = parts[0];
+                String code = parts[1];
+                languageToLanguageCode.put(language, code);
+                languageCodeToLanguage.put(code, language);
+
+
             }
 
         } catch (IOException | URISyntaxException ex) {
@@ -57,6 +64,7 @@ public class LanguageCodeConverter {
      */
     public String fromLanguageCode(String code) {
         // TODO Task A: update this code to use the correct instance variable to return the appropriate value
+        code = languageCodeToLanguage.get(code);
         return code;
     }
 
@@ -67,6 +75,7 @@ public class LanguageCodeConverter {
      */
     public String fromLanguage(String language) {
         // TODO Task A: update this code to use the correct instance variable to return the appropriate value
+        language = languageToLanguageCode.get(language);
         return language;
     }
 
